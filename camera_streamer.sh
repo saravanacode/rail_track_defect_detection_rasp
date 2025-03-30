47adbdeab02ddd7f805e16f696d05f6bc78500b6
saravanacode <141404319+saravanacode@users.noreply.github.com>
2025-03-30 18:01:18 +0530

    Update camera_streamer.sh

--- camera_streamer.sh
+++ camera_streamer.sh
@@ -12,16 +12,10 @@ WIDTH=640
 HEIGHT=480
 FRAMERATE=30
 
-# For Raspberry Pi Camera Module (using v4l2src)
+# Option 1: Using v4l2 with omxh264enc (hardware encoder for Raspberry Pi)
+# This should work on older Raspberry Pi models (3 and earlier)
 gst-launch-1.0 -v v4l2src device=/dev/video0 ! \
     video/x-raw,width=$WIDTH,height=$HEIGHT,framerate=$FRAMERATE/1 ! \
-    videoconvert ! x264enc tune=zerolatency bitrate=500 speed-preset=superfast ! \
-    rtph264pay config-interval=1 pt=96 ! \
+    videoconvert ! omxh264enc control-rate=variable target-bitrate=800000 ! \
+    h264parse ! rtph264pay config-interval=1 pt=96 ! \
     udpsink host=0.0.0.0 port=5000
-
-# Alternatively, if using the older camera interface (rpicamsrc)
-# Uncomment this and comment out the above if needed
-# gst-launch-1.0 -v rpicamsrc preview=false ! \
-#     video/x-h264,width=$WIDTH,height=$HEIGHT,framerate=$FRAMERATE/1 ! \
-#     h264parse ! rtph264pay config-interval=1 pt=96 ! \
-#     udpsink host=0.0.0.0 port=5000
